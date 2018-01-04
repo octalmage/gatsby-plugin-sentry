f4e4afd676632ca44d80c3e34cea9dffad12c1bd
Mickey Reiss <mickey@myvisor.com>
2018-01-03 17:26:06 -0800

    Clean up

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,13 +1,13 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const SentryCdn = ({ dsn, version }) =>
-  dsn !== undefined && (
+const SentryCdn = ({ dsn, version = '3.21.0' }) =>
+  dsn ? (
     <script
       src={`https://cdn.ravenjs.com/${version}/raven.min.js`}
       crossOrigin="anonymous"
     />
-  );
+  ) : null;
 
 SentryCdn.propTypes = {
   // Sentry public DSN.
@@ -16,25 +16,23 @@ SentryCdn.propTypes = {
   // If omitted, Raven.js will be excluded from the build, and Sentry will be disabled.
   dsn: PropTypes.string,
   // The version of Raven.js to load.
-  version: PropTypes.string
+  version: PropTypes.string,
 };
 
 SentryCdn.defaultProps = {
   dsn: null,
-  version: "3.21.0"
+  version: '3.21.0',
 };
 
 const SentryInstall = ({ dsn, config = {} }) => {
-  return (
-    dsn !== undefined && (
-      <script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `Raven.config('${dsn}', ${JSON.stringify(config)}).install();`
-        }}
-      />
-    )
-  );
+  return dsn ? (
+    <script
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `Raven.config('${dsn}', ${JSON.stringify(config)}).install();`,
+      }}
+    />
+  ) : null;
 };
 
 SentryInstall.propTypes = {
@@ -45,17 +43,21 @@ SentryInstall.propTypes = {
   dsn: PropTypes.string,
   // The Raven.js configuration object.
   // See: https://docs.sentry.io/clients/javascript/config/.
-  config: PropTypes.string
+  config: PropTypes.object,
 };
 
 SentryCdn.defaultProps = {
   dsn: null,
-  config: {}
+  config: {},
 };
 
 exports.onRenderBody = ({ setHeadComponents }, { version, dsn, config }) => {
   return setHeadComponents([
     <SentryCdn dsn={dsn} version={version} key="gatsby-plugin-sentry-cdn" />,
-    <SentryInstall dsn={dsn} config={config} key="gatsby-plugin-sentry-install" />
+    <SentryInstall
+      dsn={dsn}
+      config={config}
+      key="gatsby-plugin-sentry-install"
+    />,
   ]);
 };
