4a158c2167759125770f845ef5ee013d4a556d83
Thiên Lacape <thien.l@bookatiger.com>
2018-06-22 09:47:33 +0200

    Cleanup and load async

--- src/gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import Raven from "raven-js";
-
-exports.onClientEntry = (_, pluginParams) => {
-    if (window && process.env.NODE_ENV === "production" && pluginParams.dsn) {
-        window.Raven = Raven;
-        window.Raven.config(pluginParams.dsn, pluginParams.config || {}).install();
-    }
-};
